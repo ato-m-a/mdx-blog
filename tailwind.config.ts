dc8362a42a9a8a1362464284cc45e3a5ceaf6240
Tom <atomjh0295@gmail.com>
2024-10-21 08:47:09 +0900

    feat: global style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,18 +3,37 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'primary': '#333333'
       },
-    },
+      screens: {
+        'custom-lg' : '992px',
+        'max-lg': {'max': '991px'},
+        'max-md': {'max': '768px'},
+        'max-sm': {'max': '480px'}
+      },
+      container: (theme) => ({
+        center: true,
+        padding: '1rem',
+        screens : {
+          'custom-lg' : '970px'
+        },
+      }),
+      spacing: {
+        '252': '252px'
+      },
+      width: {
+        '2/3': '66.6666%',
+        '3/5': '60%'
+      }
+    }
   },
-  plugins: [],
 };
+
 export default config;
