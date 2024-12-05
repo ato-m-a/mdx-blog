08a4084259a1b02e2c7d00ccf00a204269459e7d
Tom <atomjh0295@gmail.com>
2024-12-06 07:52:08 +0900

    wip: company manage page

---
+++ app/manage/company/components/Content/index.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import type { FC } from 'react';
+
+const ManageCompanyContent: FC = () => {
+  return <main className="grid grid-cols-3 gap-6"></main>;
+};
+
+export default ManageCompanyContent;
