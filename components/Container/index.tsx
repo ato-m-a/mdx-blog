f277c7a195f5f38033d1beeeeac44024bf147cfb
Tom <atomjh0295@gmail.com>
2024-12-06 09:06:43 +0900

    refactor: components/Container에 as prop 적용

--- components/Container/index.tsx
+++ components/Container/index.tsx
@@ -1,11 +1,17 @@
-import type { FC, HTMLAttributes } from 'react';
+import type { ElementType, ReactElement } from 'react';
+import type { FlexibleComponentProps } from '@/components/types';
 import { cn } from '@/common/utils';
 
-type ContainerProps = HTMLAttributes<HTMLDivElement>;
+const Container = <T extends ElementType>({
+  children,
+  className,
+  as,
+  ...props
+}: FlexibleComponentProps<T>): ReactElement => {
+  const Component = as ?? 'div';
 
-const Container: FC<ContainerProps> = ({ children, className, ...props }) => {
   return (
-    <div
+    <Component
       className={cn(
         'container relative custom-lg mx-auto p-16 min-h-screen flex-grow max-lg:px-12 max-md:px-8 max-md:py-12 max-sm:px-6 max-sm:py-8',
         className,
@@ -13,7 +19,7 @@ const Container: FC<ContainerProps> = ({ children, className, ...props }) => {
       {...props}
     >
       {children}
-    </div>
+    </Component>
   );
 };
 
