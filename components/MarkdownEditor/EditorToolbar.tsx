5b3626a9cad1bc12d36b88074ba3a52bd09880e4
Tom <atomjh0295@gmail.com>
2024-12-08 13:19:04 +0900

    feat: MarkdownEditor/Toolbar의 최대화, 미리보기를 shadcn/toggle로 변경

--- components/MarkdownEditor/EditorToolbar.tsx
+++ components/MarkdownEditor/EditorToolbar.tsx
@@ -1,15 +1,18 @@
 'use client';
 
 import type { FC } from 'react';
+import { Toggle } from '@/components/ui/toggle';
 import { useMarkdownEditorContext } from './context';
 import { Eye, Maximize } from 'lucide-react';
 import { Skeleton } from '@/components/ui/skeleton';
-import { Button } from '@/components/ui/button';
 import { cn } from '@/common/utils';
 
 const EditorToolbar: FC = () => {
   const { isLoading, isPreviewOpen, setIsPreviewOpen, isMaximized, setIsMaximized, widget } =
     useMarkdownEditorContext();
+
+  const togglePreview = () => setIsPreviewOpen((prev) => !prev);
+  const toggleMaximize = () => setIsMaximized((prev) => !prev);
 
   if (isLoading) return <Skeleton className="w-full h-full" />;
 
@@ -17,23 +20,13 @@ const EditorToolbar: FC = () => {
     <div className="flex p-2 justify-between bg-primary items-center border-base border-b-[1px]">
       {widget}
       <div className={cn('flex items-center gap-1', !widget && 'ml-auto')}>
-        <Button
-          variant="ghost"
-          size="sm"
-          onClick={() => setIsPreviewOpen((prev) => !prev)}
-          active={isPreviewOpen}
-        >
+        <Toggle pressed={isPreviewOpen} onPressedChange={togglePreview}>
           <Eye className="w-4 h-4" />
           미리보기
-        </Button>
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={() => setIsMaximized((prev) => !prev)}
-          active={isMaximized}
-        >
+        </Toggle>
+        <Toggle pressed={isMaximized} onPressedChange={toggleMaximize}>
           <Maximize className="w-4 h-4" />
-        </Button>
+        </Toggle>
       </div>
     </div>
   );
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -27,9 +27,6 @@ const buttonVariants = cva(
         lg: 'h-10 rounded-md px-8',
         icon: 'h-9 w-9',
       },
-      active: {
-        true: 'bg-zinc-200 text-zinc-900 dark:bg-zinc-800 dark:text-zinc-50',
-      },
     },
     defaultVariants: {
       variant: 'default',
@@ -42,7 +39,7 @@ export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
   VariantProps<typeof buttonVariants> & { asChild?: boolean };
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, active = false, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button';
 
     return (
@@ -52,7 +49,6 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
             variant,
             size,
             className,
-            ...(variant === 'ghost' && { active }),
           }),
         )}
         ref={ref}
