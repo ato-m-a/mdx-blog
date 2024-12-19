0843260589337ae151f32af690bfb5309b1b7cd2
Tom <atomjh0295@gmail.com>
2024-12-19 11:16:54 +0900

    feat: sitemap이 post 색인하도록 함

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,12 +1,20 @@
 import type { MetadataRoute } from 'next';
+import { prisma } from '@/common/utils';
 
 const sitemap = async (): Promise<MetadataRoute.Sitemap> => {
+  const posts = await prisma.post.findMany();
+
   return [
     {
       url: 'https://ato-m-a.me',
       lastModified: new Date(),
-      priority: 1,
+      priority: 0.5,
     },
+    ...posts.map(({ slug, createdAt, updatedAt }) => ({
+      url: `https://ato-m-a.me/posts/${slug}`,
+      lastModified: updatedAt ?? createdAt,
+      priority: 1,
+    })),
   ];
 };
 
