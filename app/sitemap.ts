1fa96b9603d626b4a1817319d290a7b86d7be7b0
Tom <atomjh0295@gmail.com>
2024-12-29 17:58:14 +0900

    fix: posts -> post sitemap 함수 수정

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -11,12 +11,12 @@ const sitemap = async (): Promise<MetadataRoute.Sitemap> => {
       priority: 0.5,
     },
     {
-      url: 'https://ato-m-a.me/posts',
+      url: 'https://ato-m-a.me/post',
       lastModified: new Date(),
       priority: 0.5,
     },
     ...posts.map(({ slug, createdAt, updatedAt }) => ({
-      url: `https://ato-m-a.me/posts/${slug}`,
+      url: `https://ato-m-a.me/post/${slug}`,
       lastModified: updatedAt ?? createdAt,
       priority: 1,
     })),
